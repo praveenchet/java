2ca1685a3780044a9f2a09adf07e07497f8da784
praveenchet <praveensls2002@gmail.com>
2024-01-24 11:20:54 +0530

    commit print3times

---
+++ print_3_times.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void display(char name;char dept;int rollno;)
+
+{
+	for(i=1, i<=4, i++)
+		
+	
+}
+
+	int main() {
+		char name;char dept;int rollno;
+		cin>>name;
+		cin>>dept;
+		cin>>rollno;
+		cout<<name<<endl;
+		cout<<dept<<endl;
+		cout<<rollno<<endl;
+		display(char name,char dept,int rollno);
+		return 0;
+	}
+
