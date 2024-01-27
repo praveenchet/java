380d00594cecebbf036ff1fe82af99360ee81903
praveenchet <praveensls2002@gmail.com>
2024-01-27 19:41:09 +0530

    scanner input

---
+++ get_input_from_user.java
@@ -0,0 +1,19 @@
+package BASICPROJECTS;
+import java.util.*;
+public class get_input_from_user {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        int s2 = sc.nextInt();
+        float s3 = sc.nextFloat();
+        sc.nextLine();
+        String s4 = sc.nextLine();
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+	}
+
+}
